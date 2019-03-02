4cb4de0c0c55194c66f1efd059326d939027fd26
Michael Rose <Michael.Rose@theice.com>
2019-03-01 19:51:31 -0500

    Refactor robotapi unit test and add skeleton tests for missing cases

--- utest/test_robotapi.py
+++ utest/test_robotapi.py
@@ -1,37 +1,159 @@
 import os
 from django.test import TestCase
-from robot.parsing.model import TestDataDirectory, TestCaseFile
+from robot.parsing.model import TestDataDirectory
 
-from testrunner.models import RobotApplicationUnderTest
+from testrunner.models import RobotApplicationUnderTest, RobotTestSuite, RobotTest
 from robotapi.discover import DiscoveredRobotTest, DiscoveredRobotTestSuite, DiscoveredRobotApplication
 from robotapi.exceptions import RobotDiscoveryException
 
 SEP = os.path.sep
 HERE = os.path.realpath(__file__)
 UTEST_ROOT = SEP.join(HERE.split(SEP)[:-1])
+###########################################
+# If the example test suite used for testing is updated ( in TestRobotAppSuite), then the expectations for testing
+# below need to be reviewed / updated as well.
+############################################
 TEST_ROBOT_APP_DIR = SEP.join(HERE.split(SEP)[:-1]) + SEP + 'TestRobotAppSuite'
-TEST_SUITE_NAME_KEY = {  # name : verbose_name - if any are added or changed this also needs to be updated
-    'TestRobotAppSuite': 'TestRobotAppSuite',
-    'RobotAppSubDirectory': 'TestRobotAppSuite.RobotAppSubDirectory',
-    'AppSubSuite1': 'TestRobotAppSuite.AppSubSuite1',
-    'AppSubSuite2': 'TestRobotAppSuite.AppSubSuite2',
-    'NestedChildSuite': 'TestRobotAppSuite.RobotAppSubDirectory.NestedChildSuite',
-    'TemplateSubSuite': 'TestRobotAppSuite.RobotAppSubDirectory.TemplateSubSuite',
-    'AnotherTemplateTestSuite': 'TestRobotAppSuite.RobotAppSubDirectory.AnotherTemplateTestSuite',
-    'NestedTestSuite GivenWhenThen': 'TestRobotAppSuite.RobotAppSubDirectory.NestedChildSuite.'
-                                     'NestedTestSuite GivenWhenThen',
-}
-TEST_SUITE_DOC_KEY = {  # verbose_name : doc - if any are added or changed this also needs to be updated
-    'TestRobotAppSuite': '',
-    'TestRobotAppSuite.RobotAppSubDirectory': '',
-    'TestRobotAppSuite.AppSubSuite1': 'A test suite with a single test for valid login.\\n\\n'
-                                      'This test has a workflow that is created using keywords in\\n'
-                                      'the imported resource file.',
-    'TestRobotAppSuite.AppSubSuite2': 'Example using the space separated plain text format.',
-    'TestRobotAppSuite.RobotAppSubDirectory.NestedChildSuite': '',
-    'TestRobotAppSuite.RobotAppSubDirectory.TemplateSubSuite': '',
-    'TestRobotAppSuite.RobotAppSubDirectory.AnotherTemplateTestSuite': '',
-    'TestRobotAppSuite.RobotAppSubDirectory.NestedChildSuite.NestedTestSuite GivenWhenThen': '',
+TEST_SUITE_EXPECTATIONS = {
+    'TestRobotAppSuite': {
+        'Short Name': 'TestRobotAppSuite',
+        'Doc': '',
+        'Has Parent': False,
+        'Parent': None,
+        'Children': ['RobotAppSubDirectory', 'AppSubSuite1', 'AppSubSuite2'],
+        'Location': TEST_ROBOT_APP_DIR,
+        'Tests': {}
+    },
+    'TestRobotAppSuite.RobotAppSubDirectory': {
+        'Short Name': 'RobotAppSubDirectory',
+        'Doc': '',
+        'Has Parent': True,
+        'Parent': 'TestRobotAppSuite',
+        'Children': ['NestedChildSuite', 'TemplateSubSuite', 'AnotherTemplateTestSuite'],
+        'Location': TEST_ROBOT_APP_DIR + '\\RobotAppSubDirectory',
+        'Tests': {}
+    },
+    'TestRobotAppSuite.AppSubSuite1': {
+        'Short Name': 'AppSubSuite1',
+        'Doc': 'A test suite with a single test for valid login.\\n\\n'
+               'This test has a workflow that is created using keywords in\\n'
+               'the imported resource file.',
+        'Has Parent': True,
+        'Parent': 'TestRobotAppSuite',
+        'Children': [],
+        'Location': TEST_ROBOT_APP_DIR + '\\AppSubSuite1.robot',
+        'Tests': {
+            'Valid Login': {
+                'Doc': '',
+                'Steps': []
+            }
+        }
+    },
+    'TestRobotAppSuite.AppSubSuite2': {
+        'Short Name': 'AppSubSuite2',
+        'Doc': 'Example using the space separated plain text format.',
+        'Has Parent': True,
+        'Parent': 'TestRobotAppSuite',
+        'Children': [],
+        'Location': TEST_ROBOT_APP_DIR + '\\AppSubSuite2.txt',
+        'Tests': {
+            'My Test': {
+                'Doc': 'Example test',
+                'Steps': []
+            },
+            'Another Test': {
+                'Doc': '',
+                'Steps': []
+            },
+        }
+    },
+    'TestRobotAppSuite.RobotAppSubDirectory.NestedChildSuite': {
+        'Short Name': 'NestedChildSuite',
+        'Doc': '',
+        'Has Parent': True,
+        'Children': ['NestedTestSuite_GivenWhenThen'],
+        'Parent': 'RobotAppSubDirectory',
+        'Location': TEST_ROBOT_APP_DIR + '\\RobotAppSubDirectory\\NestedChildSuite',
+        'Tests': {}
+    },
+    'TestRobotAppSuite.RobotAppSubDirectory.TemplateSubSuite': {
+        'Short Name': 'TemplateSubSuite',
+        'Doc': '',
+        'Has Parent': True,
+        'Parent': 'RobotAppSubDirectory',
+        'Children': [],
+        'Location': TEST_ROBOT_APP_DIR + '\\RobotAppSubDirectory\\TemplateSubSuite.robot',
+        'Tests': {
+            'Invalid Username': {
+                'Doc': '',
+                'Steps': []
+            },
+            'Invalid Password': {
+                'Doc': '',
+                'Steps': []
+            },
+            'Invalid Username And Password': {
+                'Doc': '',
+                'Steps': []
+            },
+            'Empty Username': {
+                'Doc': '',
+                'Steps': []
+            },
+            'Empty Password': {
+                'Doc': '',
+                'Steps': []
+            },
+            'Empty Username And Password': {
+                'Doc': '',
+                'Steps': []
+            },
+        }
+    },
+    'TestRobotAppSuite.RobotAppSubDirectory.AnotherTemplateTestSuite': {
+        'Short Name': 'AnotherTemplateTestSuite',
+        'Doc': '',
+        'Has Parent': True,
+        'Parent': 'RobotAppSubDirectory',
+        'Children': [],
+        'Location': TEST_ROBOT_APP_DIR + '\\RobotAppSubDirectory\\AnotherTemplateTestSuite.robot',
+        'Tests': {
+            'Additions': {
+                'Doc': '',
+                'Steps': []
+            },
+            'Substractions': {
+                'Doc': '',
+                'Steps': []
+            },
+            'Multiplication': {
+                'Doc': '',
+                'Steps': []
+            },
+            'Division': {
+                'Doc': '',
+                'Steps': []
+            },
+            'Calculation error': {
+                'Doc': '',
+                'Steps': ''
+            }
+        }
+    },
+    'TestRobotAppSuite.RobotAppSubDirectory.NestedChildSuite.NestedTestSuite GivenWhenThen': {
+        'Short Name': 'AnotherTemplateTestSuite',
+        'Doc': '',
+        'Has Parent': True,
+        'Parent': 'NestedChildSuite',
+        'Location': UTEST_ROOT + '\\TestRobotAppSuite\\RobotAppSubDirectory\\NestedChildSuite\\'
+                                 'NestedTestSuite_GivenWhenThen.robot',
+        'Tests': {
+            'Addition': {
+                'Doc': '',
+                'Steps': []
+            }
+        }
+    },
 }
 
 
@@ -50,7 +172,7 @@ class TestDiscovery(TestCase):
                                                                      description='An application created for testing '
                                                                                  'unhappy RobotWeb discovery tools.',
                                                                      robot_location=HERE,
-                                                                     app_test_location='')
+                                                                     app_test_location='')  # This is the bad part.
 
     def test_discovered_robot_app_creation(self):
         # Expected attribute values after application under test is discovered
@@ -81,20 +203,62 @@ class TestDiscovery(TestCase):
     def test_child_suites_discovery(self):
         discovered_app = DiscoveredRobotApplication(self.test_robot_app)
         discovered_app.discover_suites_and_tests()
-        self.assertEqual(len(discovered_app.test_suites), 8)  # Total test suites discovered including the root suite.
-        actual_test_count = 0
+        self.assertEqual(len(discovered_app.test_suites), len(TEST_SUITE_EXPECTATIONS))
         for s in discovered_app.test_suites:
-            file_extension = ''
-            if not isinstance(s.suite_test_data, TestDataDirectory):    # Then it must be a TestCaseFile
-                file_extension = '.' + s.suite_test_data.source.split('.')[-1]
-            verbose_name_parts = s.name.split('.')
-            # Robot automatically replaces underscores with spaces when generating test suite names.
-            verbose_name_file_parts = [p.replace(' ', '_') for p in verbose_name_parts]
-            self.assertEqual(TEST_SUITE_NAME_KEY.get(verbose_name_parts[-1]), s.name)
-            self.assertEqual(s.location, UTEST_ROOT + SEP + SEP.join(verbose_name_file_parts) + file_extension)
-            self.assertEqual(TEST_SUITE_DOC_KEY.get(s.name), s.documentation)
-            actual_test_count += len(s.tests)
-        self.assertEqual(actual_test_count, 15)
+            self.assertIsNotNone(TEST_SUITE_EXPECTATIONS.get(s.name),
+                                 msg='Unexpected test suite discovered.')
+            self.assertEqual(TEST_SUITE_EXPECTATIONS.get(s.name)['Location'],
+                             s.location,
+                             msg='Unexpected test suite location discovered.')
+            self.assertEqual(TEST_SUITE_EXPECTATIONS.get(s.name)['Doc'],
+                             s.documentation,
+                             msg='Unexpected test suite documentation')
+            if TEST_SUITE_EXPECTATIONS.get(s.name)['Has Parent']:
+                self.assertEqual(TEST_SUITE_EXPECTATIONS.get(s.name)['Parent'], s.name.split('.')[-2])
+            else:
+                self.assertEqual(len(s.name.split('.')), 1)
+
+    def test_robot_test_discovery(self):
+        discovered_app = DiscoveredRobotApplication(self.test_robot_app)
+        discovered_app.discover_suites_and_tests()
+        for s in discovered_app.test_suites:
+            expected_tests = TEST_SUITE_EXPECTATIONS.get(s.name)['Tests']
+            self.assertEqual(len(expected_tests), len(s.tests),
+                             msg='Unexpected tests discovered in suite: ' + s.name)
+            for t in s.tests:   # DiscoveredRobotTest
+                self.assertIsInstance(t, DiscoveredRobotTest)
+                self.assertIsNotNone(expected_tests.get(t.name),
+                                     msg='Unexpected test found in {s}: {t}'.format(s=s.name, t=t.name))
+                self.assertEqual(expected_tests.get(t.name)['Doc'],
+                                 t.documentation,
+                                 msg='Discovered test documentation for {t} incorrect: {d}'.format(t=t.name,
+                                                                                                   d=t.documentation))
+
+    def test_child_test_suite_discovery_only(self):
+        discovered_app = DiscoveredRobotApplication(self.test_robot_app)
+        discovered_app.discover_suites_and_tests()
+        discovered_app.configure_suites_and_tests()     # Configure this to create saved RobotTestSuite in test DB
+        existing_robot_suite = RobotTestSuite.objects.get(name='RobotAppSubDirectory')
+        discovered_suite = DiscoveredRobotTestSuite(robot_suite=existing_robot_suite)
+        discovered_suite.discover_child_suites_and_tests()
+        self.assertIsNotNone(TEST_SUITE_EXPECTATIONS.get(discovered_suite.name),
+                             msg='Discovered child suite had an unexpected name: ' + discovered_suite.name)
+        self.assertEqual(len(TEST_SUITE_EXPECTATIONS.get(discovered_suite.name)['Children']),
+                         len(discovered_suite.child_suites),
+                         msg='Unexpected number of child suites discovered in: ' + discovered_suite.name)
+        for child in discovered_suite.child_suites:
+            child_short_name = child.name.split(existing_robot_suite.name + '.')[-1]
+            self.assertEquals(TEST_SUITE_EXPECTATIONS.get(discovered_suite.name)['Children'].count(child_short_name), 1,
+                              msg=child_short_name)
+
+    def test_remove_suite_from_app(self):
+        pass
+
+    def test_remove_child_suite_from_parent_and_app(self):
+        pass
+
+    def test_remove_test_from_suite(self):
+        pass
 
 
 class TestConfiguration(TestCase):
@@ -114,3 +278,12 @@ class TestConfiguration(TestCase):
         with self.assertRaisesMessage(RobotDiscoveryException, 'Tests and suites must be discovered before '
                                                                'they can be configured.'):
             discovered_app_suites_not_discovered.configure_suites_and_tests()
+
+    def test_configure_robot_suite(self):
+        pass
+
+    def test_configure_robot_test(self):
+        pass
+
+    def test_do_not_create_duplicate_root_suites(self):
+        pass
